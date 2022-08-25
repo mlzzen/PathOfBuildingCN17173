5f39aa77dd2871c5b49c22c12ef910b6cf4cabe5
Tai <taizhiwei16@163.com>
2022-08-26 06:49:24 +0800

    feat: 取消debug

--- Launch.lua
+++ Launch.lua
@@ -17,13 +17,13 @@ SetMainObject(launch)
 
 function launch:OnInit()
 	-- This is the path to emmy_core.dll. The ?.dll at the end is intentional.
-	package.cpath = package.cpath .. ";C:/Users/echo/.vscode/extensions/tangzx.emmylua-0.5.8/debugger/emmy/windows/x86/?.dll"
-	local dbg = require("emmy_core")
+	-- package.cpath = package.cpath .. ";C:/Users/echo/.vscode/extensions/tangzx.emmylua-0.5.8/debugger/emmy/windows/x86/?.dll"
+	-- local dbg = require("emmy_core")
 	-- This port must match the Visual Studio Code configuration. Default is 9966.
-	dbg.tcpListen("localhost", 9966)
+	-- dbg.tcpListen("localhost", 9966)
 	-- Uncomment the next line if you want Path of Building to block until the debugger is attached
 	-- dbg.waitIDE()
-	self.devMode = true
+	self.devMode = false
 	self.versionNumber = "?"
 	self.versionBranch = "?"
 	self.versionPlatform = "?"
